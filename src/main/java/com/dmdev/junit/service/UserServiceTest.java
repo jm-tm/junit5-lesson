6248c443beabcf75e72257c4514ecdb23bbfb9c7
jm-tm <jmerik.aa@gmail.com>
2021-07-09 12:03:03 +0300

    start

---
+++ src/main/java/com/dmdev/junit/service/UserServiceTest.java
@@ -0,0 +1,14 @@
+package com.dmdev.junit.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class UserServiceTest {
+
+    @Test
+    void test(){
+        assertTrue(false);
+
+    }
+}
